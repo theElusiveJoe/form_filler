ab85b1a188696273055d8bf185b131821c5a4e9d
theElusiveJoe <flvlimpo@gmail.com>
2022-03-20 12:58:28 +0300

    startswith update

--- compose_data/compose_info.py
+++ compose_data/compose_info.py
@@ -5,9 +5,10 @@ from compose_data.gsheets_query import get_order_from_gsheetstring
 from compose_data.zippack_query import get_order_from_zippack
 from compose_data.mail_reader import get_order_from_mail
 
+
 def get_order_info(line_num):
     gstring, order_id = get_order_from_gsheetstring(line_num)
-    if order_id.startswith('Z') or order_id.startswith('Y'):
+    if ord('A') <= ord(order_id[0]) <= ord('Z'): 
         zstring = get_order_from_zippack(re.sub("\D", "", order_id))
     else:
         zstring = get_order_from_mail(order_id)
