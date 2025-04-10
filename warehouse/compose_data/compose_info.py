37ce945191299d6333a87908d46152c9ebe1ad1c
Your Name Here <your@email.example>
2025-04-10 23:54:24 +0300

    какой даун это говно писал? а. это был я

--- warehouse/compose_data/compose_info.py
+++ warehouse/compose_data/compose_info.py
@@ -5,6 +5,7 @@ import logging
 from compose_data.gsheets_query import get_order_from_gsheetstring
 from compose_data.zippack_query import get_order_from_zippack
 from compose_data.mail_reader import get_order_from_mail
+from compose_data.spbkonvert_query import get_order_from_spbkonvert
 
 
 def get_order_info(line_num):
@@ -16,17 +17,12 @@ def get_order_info(line_num):
         except:
             logging.exception('')
             logging.error('Проблема с гугл таблицами')
-            zstring = '""'
-       
     else:
         try:
-            zstring = get_order_from_mail(order_id)
+            zstring = get_order_from_spbkonvert(order_id)
         except:
             logging.exception('')
-            logging.error('Проблема с почтой')
-    
-    if zstring.strip() == '':
-        zstring = '{}'
+            logging.error('Проблема с спбконверт')
     the_string = f'{{"zippack":{zstring}, "gsheets":{gstring}}}'
     
     print('--------------------ПРИШЛО С ЗАППАКА|ПОЧТЫ:--------------------\n' + zstring)
